33881c53a4560d3d213532eabe4a842d8579fb7f
hamt3ch <hmiles23@ufl.edu>
2017-01-25 12:40:29 -0800

    add endpoint to index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,13 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
 import ride from './ride';
+import user from './user';
 
 export default ({ config, db }) => {
 	let api = Router();
 
 	api.use('/rides', ride({ config, db }));
+	api.use('/user', user({ config, db }));
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
