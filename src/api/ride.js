654303b5f060dde376f2fe0ffcd4a0b3e4d442a8
hamt3ch <hmiles23@ufl.edu>
2017-01-26 21:24:49 -0800

    cleanup for ES6 standards

--- src/api/ride.js
+++ src/api/ride.js
@@ -3,6 +3,71 @@ import resource from 'resource-router-middleware';
 import Ride from '../models/ride';
 import { geocoder } from '../service';
 
+// Helper Methods
+const requestValid = (body) => {
+  let count = 0;
+  for (var field in body) {
+    // check if any fields are empty
+    if (body[field] === '') {
+      return false;
+    }
+    count += 1;
+  }
+
+  // make sure date is valid
+  if (!moment(body.departure_time, 'MM-DD-YYYY h:mm a').isValid()) {
+    return false;
+  }
+
+  // make sure all fields are accounted for
+  if (count !== 5) {
+    return false;
+  }
+
+  return true;
+};
+
+const geocodeAddress = (addr, callback) => {
+  geocoder.batchGeocode(addr)
+  .then((res) => {
+    callback({
+      departure: {
+        latitude: res[0].value[0].latitude,
+        longitude: res[0].value[0].longitude,
+      },
+      arrival: {
+        latitude: res[1].value[0].latitude,
+        longitude: res[1].value[0].longitude,
+      },
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+};
+
+const configureBody = (body, data) => {
+  // Transform date.string to actual date type
+  const dateString = body.departure_time;
+  const convert = moment(dateString, 'MM-DD-YYYY h:mm a');
+  const response = body;
+  response.departure_time = convert;
+
+  // Get long and lat for arrival and depart address
+  geocodeAddress([response.departure_location, response.arrival_location], (callback) => {
+    response.departure_longitude = callback.departure.longitude;
+    response.departure_latitude = callback.departure.latitude;
+    response.arrival_longitude = callback.arrival.longitude;
+    response.arrival_latitude = callback.arrival.latitude;
+    data(body);
+  });
+};
+
+// TODO: fill out error handling for all request
+const handleError = (error) => {
+  console.log(error);
+};
+
 export default ({ config, db }) => resource({
 
 /** Property name to store preloaded entity on `request`. */
@@ -12,115 +77,48 @@ export default ({ config, db }) => resource({
 *  Errors terminate the request, success sets `req[id] = data`.
 */
   load(req, id, callback) {
-		// let facet = facets.find( facet => facet.id===id ),
-		// 	err = facet ? null : 'Not found';
-		// callback(err, facet);
-	},
+  // let facet = facets.find( facet => facet.id===id ),
+  // err = facet ? null : 'Not found';
+  // callback(err, facet);
+  },
 
-	/** GET / - List all entities */
-	index({ params }, res) {
-		Ride.where('createdBy')
-		.exec((err, rides) => {
-			if (err) return handleError(err);
-			res.json(rides);
-		})
-	},
+  /** GET / - List all entities */
+  index({ params }, res) {
+    Ride.where('createdBy')
+      .exec((err, rides) => {
+        if (err) return handleError(err);
+        return res.json(rides);
+      });
+  },
 
-	/** POST / - Create a new entity */
-	create({ body }, res) {
-		if(requestValid(body)) {
-			configureBody(body, (data) => {
-				var rideToSave = new Ride(data);
-				rideToSave.save((err) => { // problem saving data to db
-					if (err) return console.log(err);
-				})
+  /** POST / - Create a new entity */
+  create({ body }, res) {
+    if (requestValid(body)) {
+      configureBody(body, (data) => {
+        const rideToSave = new Ride(data);
+        rideToSave.save((err) => { // problem saving data to db
+          if (err) return console.log(err);
+          return true;
+        });
+        res.json(rideToSave); // Send back ride.json for confirmation
+      });
+    } else {
+      res.json({ error: 'misformatted body' });
+    }
+  },
 
-				res.json(rideToSave); // Send back ride.json for confirmation
+  /** GET /:id - Return a given entity */
+  read({ facet }, res) {
+    res.json(facet);
+  },
 
-			});
-		} else {
-			res.json({'error': 'misformatted body'});
-		}
-	},
+  /** PUT /:id - Update a given entity */
+  update({ facet, body }, res) {
+    res.sendStatus(204);
+  },
 
-	/** GET /:id - Return a given entity */
-	read({ facet }, res) {
-		res.json(facet);
-	},
-
-	/** PUT /:id - Update a given entity */
-	update({ facet, body }, res) {
-		for (let key in body) {
-			if (key!=='id') {
-				facet[key] = body[key];
-			}
-		}
-		res.sendStatus(204);
-	},
-
-	/** DELETE /:id - Delete a given entity */
-	delete({ facet }, res) {
-		facets.splice(facets.indexOf(facet), 1);
-		res.sendStatus(204);
-	}
+  /** DELETE /:id - Delete a given entity */
+  delete({ facet }, res) {
+    res.sendStatus(204);
+  },
 });
-
-// Helper Methods
-var requestValid = (body) => {
-	var count = 0;
-	for(var field in body){
-		//check if any fields are empty
-		if(body[field] === ''){
-			return false;
-		}
-		count++;
-	}
-
-	// make sure date is valid
-	console.log(body.departure_time);
-	if(!moment(body.departure_time, "MM-DD-YYYY h:mm a").isValid()){
-		return false;
-	}
-
-	// make sure all fields are accounted for
-	if(count !== 5){
-		return false;
-	}
-
-	return true;
-}
-
-var configureBody = (body, data) => {
-	// Transform date.string to actual date type
-	var dateString = body.departure_time;
-	var convert = moment(dateString, "MM-DD-YYYY h:mm a");
-	body.departure_time = convert;
-
-	// Get long and lat for arrival and depart address
-	var locations = geocodeAddress([body.departure_location,body.arrival_location], (callback) => {
-		body.departure_longitude = callback.departure.longitude;
-		body.departure_latitude = callback.departure.latitude;
-		body.arrival_longitude = callback.arrival.longitude;
-		body.arrival_latitude = callback.arrival.latitude;
-		data(body);
-	});
-}
-
-var geocodeAddress = (addr,callback) => {
-	geocoder.batchGeocode(addr)
-	.then(function(res) {
-		callback({
-			"departure": {
-				"latitude":res[0].value[0].latitude,
-				"longitude": res[0].value[0].longitude
-			},
-			"arrival": {
-				"latitude": res[1].value[0].latitude,
-				"longitude": res[1].value[0].longitude
-			}
-		})
-	})
-	.catch(function(err) {
-		console.log(err);
-	});
-}
