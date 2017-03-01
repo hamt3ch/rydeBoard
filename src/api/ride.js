11105f2a832161e9b0fba7a9466ac3817592f276
psdewar2 <psdewar2@live.com>
2017-02-28 22:58:18 -0800

    Updated /rides to distinguish errors

--- src/api/ride.js
+++ src/api/ride.js
@@ -7,16 +7,10 @@ import { Ride } from '../models';
 /**
  * Helper methods
  */
-const validateRideObject = (body) => {
-  if (!Util.allFieldsValid(body)) {
-    console.log('A field is blank.');
-  }
-  if (!moment(body.departure_time, 'MM-DD-YYYY h:mm a').isValid()) {
-    console.log('Departure time is invalid.');
-    return false;
-  }
-  return true;
-};
+
+const noEmptyFields = body => Util.allFieldsValid(body);
+
+const timeIsValid = body => moment(body.departure_time, 'MM-DD-YYYY h:mm a').isValid();
 
 const convertAddress = (address, callback) => {
   geocoder.batchGeocode(address)
@@ -87,7 +81,15 @@ export default ({ config, db }) => resource({  // eslint-disable-line
 
   /** POST / - Create new ride */
   create({ body }, response) {
-    if (validateRideObject(body)) {
+    if (!noEmptyFields(body)) {
+      response.json({
+        response: 'A field is empty.',
+      });
+    } else if (!timeIsValid(body)) {
+      response.json({
+        response: 'Please format time correctly.',
+      });
+    } else {
       configureBody(body, (data) => {
         const rideToSave = new Ride(data);
         rideToSave.save((err) => { // problem saving data to db
@@ -95,11 +97,6 @@ export default ({ config, db }) => resource({  // eslint-disable-line
           return true;
         });
         response.json(rideToSave); // Send back ride.json for confirmation
-      });
-    } else {
-      console.log(response);
-      response.json({
-        response: 'invalid ride object',
       });
     }
   },
