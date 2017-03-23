77cc3d4a9f02c79cb3f48d074490e983abe17e03
hamt3ch <hmiles23@ufl.edu>
2017-03-22 23:40:23 -0700

    added test

--- src/api/ride.js
+++ src/api/ride.js
@@ -33,8 +33,9 @@ const convertAddress = (address, callback) => {
 
 const configureBody = (body, data) => {
   // Transform date.string to actual date type
-  const dateString = body.departure_time;
-  const convert = moment(dateString + ' +0000', 'MM-DD-YYYY h:mm a Z');
+  const tz = '+0000';
+  const dateString = `${body.departure_time} ${tz}`;
+  const convert = moment(dateString, 'MM-DD-YYYY h:mm a Z');
   const response = body;
   response.departure_time = convert;
 
